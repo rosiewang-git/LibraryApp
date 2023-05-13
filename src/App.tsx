13347499955c43b23d36b38c7c7b68fcbbde0672
ywan168 <54960821+ywan168@users.noreply.github.com>
2023-05-13 09:33:08 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Routes, Route, Navigate } from "react-router";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => {
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <Navbar />
+            <div className="flex-grow-1">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/search" element={<SearchBooksPage />} />
+                    <Route
+                        path="/checkout/:bookId"
+                        element={<BookCheckoutPage />}
+                    />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
+};
